734a26f20f1e4fd079afbcc87dbf61dde16d5cfa
Spectorclef <sparksailor7@gmail.com>
2024-11-13 02:12:52 +0100

    add storage backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,13 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
+import { storage } from './storage/resource';
+
+
+
+
 defineBackend({
   auth,
   data,
+  storage
 });
